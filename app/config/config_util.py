abd1e5db9c28546c71e4ba9291f61bb728251f7b
晓哥 <5158404+yubx@user.noreply.gitee.com>
2020-09-10 18:29:51 +0800

    signature

--- app/config/config_util.py
+++ app/config/config_util.py
@@ -6,12 +6,11 @@ root_dir = os.path.dirname(os.path.abspath('.'))
 # get project cofnig data
 def get_value(section, key):
     cf = configparser.ConfigParser()
-    cf.read(root_dir + "/config.ini")
+    cf.read(root_dir + "/config/config.ini")
     value = cf.get(section, key)
-    print(value)
     return value
 
 
 if __name__ == '__main__':
-    a = get_value('token', 'token')
+    a = get_value('signature', 'signature')
     print(a)
